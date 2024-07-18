1b288b6650096131522bd82161712d2cef7521de
Emanuelle de Carvalho Brito <emanuellybritto95@gmail.com>
2024-07-18 16:33:06 -0300

    mensagem

---
+++ exercicios.js
@@ -0,0 +1,46 @@
+// Crie um objeto para representar um aluno, incluindo propriedades como nome, idade, curso e notas. Popule o objeto com valores e imprima os dados do aluno.
+
+// const aluno = {
+//     nome: 'Emanuelle',
+//     idade: 12,
+//     curso: 'ADS',
+//     notas : [10, 10, 10]
+// }
+
+// console.log(aluno)
+
+
+// Crie um objeto carro com as propriedades marca, modelo e ano. Em seguida, imprima a marca do carro.
+
+// const carro = {
+//     marca: 'Honda',
+//     modelo: 'Civic',
+//     ano: 2020
+// }
+
+// console.log(carro)
+// console.log('Marca:', carro.marca)
+
+// Crie um objeto livro com as propriedades titulo, autor e anoPublicacao. Imprima o título e o autor do livro.
+
+// const livro = {
+//     titulo: 'As besteiras de Letícia',
+//     autor: 'Mary Englads',
+//     anoPublicacao: 1972 
+// }
+
+// console.log(livro)
+// console.log('Título:', livro.titulo)
+// console.log('Autor:', livro.autor)
+
+// Crie um objeto produto com as propriedades nome, preco e quantidade. Em seguida, calcule e imprima o valor total do estoque (preço * quantidade).
+
+// const produto = {
+//     nome: 'Bolo de chocolate',
+//     preco: 24.00,
+//     quantidade: 5
+// }
+
+// let estoque = (produto.preco * produto.quantidade)
+
+// console.log('Valor total do estoque: ', estoque)
